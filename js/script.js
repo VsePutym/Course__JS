007b4a03ce452d806ee2417a4f4d0bd5215a809c
Vse Putyom <bertraid777@gmail.com>
2021-02-01 22:18:00 +0300

    fix(script.js): fix bug in script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,40 @@
-'use string';
+'use strict';
 
 let money = 70000;
 let income = 'freelance';
 let addExpenses = 'Cat, Shope, Store, Taxi, Education';
 let deposit = 10000;
-let mission = 1000000;
-let period = 12;
+let mission = 250000;
+let period = 10;
 let budgetDay = null;
 let dayInMonth = 30;
 let expenses1 = prompt('Введите обязательную статью расходов?' + '');
 let amount1 = +prompt('Во сколько это обойдется?' + '');
 let expenses2 = prompt('Введите обязательную статью расходов?' + '');
 let amount2 = +prompt('Во сколько это обойдется?' + '');
+let consumption = amount1 + amount2;
+
 
 budgetDay = money / dayInMonth;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+
 money = +prompt('ваш месячный доход?' + '');
 addExpenses = prompt('Перечислите возможные расходы за расчитываемый период через запятую' + '');
+console.log(addExpenses.toLowerCase().split(', '));
 deposit = confirm('Есть ли у вас депозит в банке?');
-    let budgetMonth = amount1 + amount2;
-    console.log(budgetMonth);
-    const howMonth = mission / budgetMonth;
-    console.log(Math.ceil(howMonth));
-    budgetDay = howMonth / budgetDay;
-    console.log(Math.floor(budgetDay));
+console.log(typeof deposit);
+    let budgetMonth = money - consumption;
+    console.log('ваш месячный бюджет ' + budgetMonth);
+    const howMonth = budgetMonth * period / mission;
+    console.log('цель бюджета достигнута за ' + (Math.ceil(howMonth) +  ' месяцев'));
+    budgetDay = howMonth * dayInMonth * budgetDay / dayInMonth;
+    console.log('бюджет на день ' + (Math.floor(budgetDay)));
 
     if( budgetDay > 1200){
         alert('у вас высокий уровень дохода');
