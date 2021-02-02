bfa8ddfd3c2691bb5b57f007db7b6042d3488046
Vse Putyom <bertraid777@gmail.com>
2021-02-02 20:27:24 +0300

    feat(script.js): functions have been added

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 let money = +prompt('your monthly income?', 70000);
 let income = 'freelance';
-let addExpenses = prompt('List the possible expenses for the calculated period, separated by commas');
+let addExpenses = prompt('List the possible expenses for the calculated period, separated by commas', );
 let deposit = confirm('Do you have a bank deposit?');
 let mission = 250000;
 let period = 10;
@@ -11,27 +11,48 @@ let expenses1 = prompt('Enter the required expense item', 'Transport');
 let amount1 = +prompt('How much will it cost?', 2500);
 let expenses2 = prompt('Enter the required expense item', 'Food');
 let amount2 = +prompt('How much will it cost?', 13000);
-let consumption = amount1 + amount2;
-let budgetMonth = money - consumption;
-let budgetDay = budgetMonth / dayInMonth;
+let accumulatedMonth;
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(addExpenses.length);
-console.log('The period is ' + period + ' months');
-console.log('Purpose to earn ' + mission + ' rubles');
-console.log(addExpenses.toLowerCase().split(', '));
-console.log(typeof deposit);
-console.log('your monthly budget ' + budgetMonth);
-console.log('budget target achieved in ' + (Math.ceil(mission / budgetMonth) +  ' months'));
-console.log('budget for the day ' + (Math.floor(budgetDay)));
+addExpenses = addExpenses.split(',');
 
+function getExpensesMonth(a, b){
+    return a + b;
+}
+getExpensesMonth(amount1, amount2);
+
+function getAccumulatedMonth(a, b){
+    return a - b;
+}
+accumulatedMonth = getAccumulatedMonth(money, getExpensesMonth(amount1, amount2));
+
+let budgetDay = accumulatedMonth / dayInMonth;
+
+function getTargetMonth(arg1, arg2){
+    return arg1 / arg2;
+}
+getTargetMonth(mission, accumulatedMonth);
+
+function showTypeOf(data){
+    console.log(data, typeof(data));
+}
+
+showTypeOf('your monthly expenses ' + getExpensesMonth(amount1, amount2));
+showTypeOf('your possible expenses ' + addExpenses);
+showTypeOf('budget target achieved in ' + Math.ceil(getTargetMonth(mission, accumulatedMonth)));
+showTypeOf('budget for the day ' + Math.floor(budgetDay));
+
+
+let getStatusIncome = function(){
     if( budgetDay >= 1200){
-        alert('you have a high income level');
+        return('you have a high income level');
     }else if( budgetDay >= 600 && budgetDay <= 1200){
-        alert('You have an average income');
+        return('You have an average income');
     }else if( budgetDay <= 600 && budgetDay >= 0){
-        alert('Unfortunately your income is below average');
+        return('Unfortunately your income is below average');
     }else if( budgetDay <= 0 ){
-        alert('Something went wrong');
+        return('Something went wrong');
     }
+};
+
+getStatusIncome();
+console.log(getStatusIncome());
