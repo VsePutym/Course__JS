e68669918e0af0d60874704701b420b727e644bd
Vse Putyom <bertraid777@gmail.com>
2021-02-02 12:36:13 +0300

    fix(script.js): removed some variables, fixed language

--- js/script.js
+++ js/script.js
@@ -1,41 +1,37 @@
 'use strict';
 
-let money = +prompt('ваш месячный доход?');
+let money = +prompt('your monthly income?', 70000);
 let income = 'freelance';
-let addExpenses = prompt('Перечислите возможные расходы за расчитываемый период через запятую');
-let deposit = confirm('Есть ли у вас депозит в банке?');
+let addExpenses = prompt('List the possible expenses for the calculated period, separated by commas');
+let deposit = confirm('Do you have a bank deposit?');
 let mission = 250000;
 let period = 10;
-let budgetDay = null;
 let dayInMonth = 30;
-let expenses1 = prompt('Введите обязательную статью расходов?');
-let amount1 = +prompt('Во сколько это обойдется?');
-let expenses2 = prompt('Введите обязательную статью расходов?');
-let amount2 = +prompt('Во сколько это обойдется?');
+let expenses1 = prompt('Enter the required expense item', 'Transport');
+let amount1 = +prompt('How much will it cost?', 2500);
+let expenses2 = prompt('Enter the required expense item', 'Food');
+let amount2 = +prompt('How much will it cost?', 13000);
 let consumption = amount1 + amount2;
-
-budgetDay = money / dayInMonth;
 let budgetMonth = money - consumption;
-const howMonth = budgetMonth * period / mission;
-budgetDay = howMonth * dayInMonth * budgetDay / dayInMonth;
+let budgetDay = budgetMonth / dayInMonth;
 
 console.log(typeof money);
 console.log(typeof income);
 console.log(addExpenses.length);
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей');
+console.log('The period is ' + period + ' months');
+console.log('Purpose to earn ' + mission + ' rubles');
 console.log(addExpenses.toLowerCase().split(', '));
 console.log(typeof deposit);
-console.log('ваш месячный бюджет ' + budgetMonth);
-console.log('цель бюджета достигнута за ' + (Math.ceil(howMonth) +  ' месяцев'));
-console.log('бюджет на день ' + (Math.floor(budgetDay)));
+console.log('your monthly budget ' + budgetMonth);
+console.log('budget target achieved in ' + (Math.ceil(budgetMonth * period / mission) +  ' months'));
+console.log('budget for the day ' + (Math.floor(budgetDay)));
 
     if( budgetDay > 1200){
-        alert('у вас высокий уровень дохода');
+        alert('you have a high income level');
     }else if( budgetDay > 600 && budgetDay < 1200){
-        alert('У вас средний уровень дохода');
+        alert('You have an average income');
     }else if( budgetDay < 600 && budgetDay > 0){
-        alert('К сожалению у вас уровень дохода ниже среднего');
+        alert('Unfortunately your income is below average');
     }else if( budgetDay <= 0 ){
-        alert('Что то пошло не так');
+        alert('Something went wrong');
     }
